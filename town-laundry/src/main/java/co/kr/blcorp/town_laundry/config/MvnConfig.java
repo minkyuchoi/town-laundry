c05cb2da0f2dce73b325eaf317a6a61ecbf6a9e1
minkyuchoi <choiminkyu@gmail.com>
2016-06-10 01:24:46 +0900

    0.0.1-Initial environment setup (Spring boot & Security & thymeleaf)

---
+++ town-laundry/src/main/java/co/kr/blcorp/town_laundry/config/MvnConfig.java
@@ -0,0 +1,18 @@
+package co.kr.blcorp.town_laundry.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvnConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
